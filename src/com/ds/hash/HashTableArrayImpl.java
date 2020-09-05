869a523e303c6631932dc438f3f74e88fba4451c
Vivek pc <Vivek pc@Vivek>
2020-09-05 10:14:03 +0530

    implementation altered

--- src/com/ds/hash/HashTableArrayImpl.java
+++ src/com/ds/hash/HashTableArrayImpl.java
@@ -3,8 +3,15 @@ package com.ds.hash;
 public class HashTableArrayImpl {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HashTableArray<String> hm = new HashTableArray<String>(10);
+		hm.put(10, "Vivek");
+		hm.put(11, "pallavi");
+		hm.put(12, "susmita");
+		hm.put(13, "pratik");
 
+		System.out.println(hm.get(10));
+		System.out.println(hm.get(9));
+		System.out.println(hm.get(13));
 	}
 
 }
